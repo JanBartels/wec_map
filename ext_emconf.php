c6f55087887e48835f145e798da42aa01c8c95cf
jbartels <jbartels@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-07-20 22:40:42 +0000

    Release 3.1.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/wec_map/trunk@85984 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Mapping extension that connects to geocoding databases and Google Maps API.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '3.1.0',
+	'version' => '3.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => 'bottom',
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '3.0.0-0.0.0',
+			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.2.0-6.2.99',
 		),
 		'conflicts' => array(
@@ -42,7 +42,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:70:{s:9:"CHANGELOG";s:4:"f9d7";s:27:"class.tx_wecmap_backend.php";s:4:"abac";s:32:"class.tx_wecmap_batchgeocode.php";s:4:"dfbd";s:25:"class.tx_wecmap_cache.php";s:4:"8f1f";s:29:"class.tx_wecmap_domainmgr.php";s:4:"117d";s:23:"class.tx_wecmap_map.php";s:4:"39f4";s:26:"class.tx_wecmap_marker.php";s:4:"ef39";s:31:"class.tx_wecmap_markergroup.php";s:4:"6d99";s:26:"class.tx_wecmap_shared.php";s:4:"40be";s:40:"class.tx_wecmap_staticinfotables_div.php";s:4:"a8fd";s:16:"ext_autoload.php";s:4:"9c95";s:21:"ext_conf_template.txt";s:4:"b074";s:12:"ext_icon.gif";s:4:"91f0";s:17:"ext_localconf.php";s:4:"1f9a";s:14:"ext_tables.php";s:4:"62a3";s:14:"ext_tables.sql";s:4:"a3f7";s:16:"locallang_db.xml";s:4:"49cd";s:7:"tca.php";s:4:"77b4";s:29:"contrib/tablesort/fastinit.js";s:4:"afbd";s:30:"contrib/tablesort/tablesort.js";s:4:"c6e0";s:24:"csh/locallang_csh_ff.xml";s:4:"cf9e";s:14:"doc/manual.sxw";s:4:"def8";s:50:"geocode_service/class.tx_wecmap_geocode_google.php";s:4:"01aa";s:14:"images/aai.gif";s:4:"03ce";s:20:"images/icon_home.gif";s:4:"6e80";s:27:"images/icon_home_shadow.png";s:4:"ce1c";s:20:"images/mm_20_red.png";s:4:"453d";s:23:"images/mm_20_shadow.png";s:4:"f77b";s:49:"map_service/google/class.tx_wecmap_map_google.php";s:4:"ca3f";s:52:"map_service/google/class.tx_wecmap_marker_google.php";s:4:"eef9";s:32:"map_service/google/locallang.xml";s:4:"a6a2";s:13:"mod1/ajax.php";s:4:"58be";s:38:"mod1/class.tx_wecmap_recordhandler.php";s:4:"418b";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"b547";s:14:"mod1/index.php";s:4:"4a13";s:18:"mod1/locallang.xml";s:4:"6517";s:22:"mod1/locallang_mod.xml";s:4:"cd7f";s:19:"mod1/moduleicon.gif";s:4:"1af1";s:14:"mod2/clear.gif";s:4:"cc11";s:13:"mod2/conf.php";s:4:"5da6";s:14:"mod2/index.php";s:4:"8e2a";s:18:"mod2/locallang.xml";s:4:"62bf";s:22:"mod2/locallang_mod.xml";s:4:"acf5";s:19:"mod2/moduleicon.gif";s:4:"4fd7";s:14:"pi1/ce_wiz.gif";s:4:"fa31";s:27:"pi1/class.tx_wecmap_pi1.php";s:4:"d77d";s:35:"pi1/class.tx_wecmap_pi1_wizicon.php";s:4:"0850";s:19:"pi1/flexform_ds.xml";s:4:"0824";s:17:"pi1/locallang.xml";s:4:"c370";s:20:"pi1/static/setup.txt";s:4:"7f68";s:14:"pi2/ce_wiz.gif";s:4:"4083";s:27:"pi2/class.tx_wecmap_pi2.php";s:4:"f3d9";s:35:"pi2/class.tx_wecmap_pi2_wizicon.php";s:4:"7262";s:19:"pi2/flexform_ds.xml";s:4:"2bac";s:17:"pi2/locallang.xml";s:4:"3978";s:20:"pi2/static/setup.txt";s:4:"6e2c";s:14:"pi3/ce_wiz.gif";s:4:"e7bd";s:27:"pi3/class.tx_wecmap_pi3.php";s:4:"77e8";s:35:"pi3/class.tx_wecmap_pi3_wizicon.php";s:4:"0775";s:19:"pi3/flexform_ds.xml";s:4:"0463";s:17:"pi3/locallang.xml";s:4:"ed1a";s:20:"pi3/static/setup.txt";s:4:"1fa5";s:17:"res/copyrights.js";s:4:"09e8";s:40:"res/icon_tx_wecmap_external_resource.gif";s:4:"daf0";s:13:"res/wecmap.js";s:4:"3979";s:21:"res/wecmap_backend.js";s:4:"dec6";s:16:"static/setup.txt";s:4:"bfd1";s:27:"tests/autozoom_testcase.php";s:4:"42c8";s:36:"tests/get_address_field_testcase.php";s:4:"07dc";}',
 	'suggests' => array(
 	),
 );
